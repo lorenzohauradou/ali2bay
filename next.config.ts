c20743e6eb076fd8c32f5edf053bec6832214edf
Lorenzo1799 <lorenzogwada@gmail.com>
2025-02-16 23:08:03 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/collega-ebay',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/collega-ebay`
+      }
+    ]
+  }
 };
 
 export default nextConfig;
