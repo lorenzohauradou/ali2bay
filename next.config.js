48ddf13584d2bda79c68af2a77347535805c88fb
Lorenzo1799 <lorenzogwada@gmail.com>
2025-02-17 02:36:42 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001'
+    
     return [
       {
         source: '/collega-ebay',
-        destination: 'http://localhost:5001/collega-ebay',
+        destination: `${apiUrl}/collega-ebay`,
       },
       {
         source: '/check-ebay-status',
-        destination: 'http://localhost:5001/check-ebay-status',
+        destination: `${apiUrl}/check-ebay-status`,
       },
       {
         source: '/ebay-callback',
-        destination: 'http://localhost:5001/ebay-callback',
+        destination: `${apiUrl}/ebay-callback`,
       },
     ]
   },
