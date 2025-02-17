36f6fa0e27a463aa0740682498b866f555ff7871
Lorenzo1799 <lorenzogwada@gmail.com>
2025-02-17 20:09:04 +0100

    .

--- config/api.ts
+++ config/api.ts
@@ -1,7 +1,7 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || (
   process.env.NODE_ENV === 'development' 
     ? 'http://localhost:5001'  
-    : 'https://api.ali2bay.com' 
+    : 'https://ali2bay.com' 
 );
 
 export const fetchApi = async (endpoint: string, options: RequestInit = {}) => {
